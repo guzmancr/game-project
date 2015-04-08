01cd1f6a808eb45a3f81f5eeedadb80409064943
Cristian Guzman <guzmancr@usc.edu>
2015-04-07 23:03:58 -0700

    Create Action.cpp

---
+++ Action.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "bank.h"
+#include "Player.h"
+#include "Action.h"
+
+using namespace std;
+
+ Action::Action()
+ {
+OK=" ";
+
+ }
+
+ void Action::writeAction(string a, string b, string c )
+ {
+
+
+ }
+ 
+
+ 
